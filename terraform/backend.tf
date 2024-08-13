300152b491dd50683fc5a1b7e9e5fe28447d3130
johnbedeir <john.bedeir@gmail.com>
2024-08-13 11:41:59 +0200

    project refactor#2

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,9 +1,9 @@
-# terraform {
-#   backend "s3" {
-#     bucket         = "mytfstate-bucket"
-#     key            = "terraform.tfstate"
-#     region         = "eu-central-1"
-#     encrypt        = true 
-#     dynamodb_table = "terraform-lock-table"
-#   }
-# }
+terraform {
+  backend "s3" {
+    bucket         = "mytfstate-bucket"
+    key            = "terraform.tfstate"
+    region         = "eu-central-1"
+    encrypt        = true 
+    dynamodb_table = "terraform-lock-table"
+  }
+}
